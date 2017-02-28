6390f018754c116889c676966914ea67124afe09
Shane Lillie <ignignokterr@gmail.com>
2017-02-28 12:08:07 -0800

    More auth progress

--- Netrunner-Unity/Assets/Scripts/Auth/AuthManager.cs
+++ Netrunner-Unity/Assets/Scripts/Auth/AuthManager.cs
@@ -1,9 +1,34 @@
-﻿using EnergonSoftware.Netrunner.Core;
+﻿using System;
+
+using EnergonSoftware.Netrunner.Core;
+using EnergonSoftware.Netrunner.Core.Util;
+
+using UnityEngine;
 
 namespace EnergonSoftware.Netrunner
 {
     public sealed class AuthManager : SingletonBehavior<AuthManager>
     {
-        public bool IsAuthenticated => false;
+        [SerializeField]
+        [ReadOnly]
+        private bool _isAuthenticated;
+
+        public bool IsAuthenticated => _isAuthenticated;
+
+        public void Authenticate(string username, string password, bool saveLogin, Action onSuccess, Action<string> onFailure)
+        {
+            if(saveLogin) {
+                PlayerPrefs.SetString("authUsername", username);
+                // TODO: password
+            } else {
+                PlayerPrefs.DeleteKey("authUsername");
+                // TODO: password
+            }
+            PlayerPrefsExtensions.SetBool("authSaveLogin", saveLogin);
+
+// TODO: put this shiz in a callback to the auth process
+            _isAuthenticated = true;
+            onSuccess?.Invoke();
+        }
     }
 }
--- Netrunner-Unity/Assets/Scripts/UI/Authentication.cs
+++ Netrunner-Unity/Assets/Scripts/UI/Authentication.cs
@@ -1,22 +1,71 @@
 ﻿using EnergonSoftware.Netrunner.Core;
+using EnergonSoftware.Netrunner.Core.Util;
 
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 namespace EnergonSoftware.Netrunner.UI
 {
     public sealed class Authentication : MonoBehavior
     {
+#region Controls
+        #pragma warning disable 0649
         [SerializeField]
         private InputField _usernameInput;
 
         [SerializeField]
         private InputField _passwordInput;
 
+        [SerializeField]
+        private Toggle _saveLoginToggle;
+
+        [SerializeField]
+        private Core.UI.Text _errorText;
+
+        [SerializeField]
+        private Button _loginButton;
+        #pragma warning restore 0649
+#endregion
+
+        private void Awake()
+        {
+            _errorText.enabled = false;
+        }
+
+        private void Start()
+        {
+            _usernameInput.text = PlayerPrefs.GetString("authUsername");
+            // TODO: password
+            _saveLoginToggle.isOn = PlayerPrefsExtensions.GetBool("authSaveLogin", true);
+        }
+
+        private void SetInteractable(bool interactable)
+        {
+            _usernameInput.interactable = interactable;
+            _passwordInput.interactable = interactable;
+            _saveLoginToggle.interactable = interactable;
+            _loginButton.interactable = interactable;
+        }
+
 #region Event Handlers
         public void OnLogin()
         {
-Debug.Log("login " + _usernameInput.text);
+            SetInteractable(false);
+            _errorText.text = string.Empty;
+
+            AuthManager.Instance.Authenticate(_usernameInput.text, _passwordInput.text, _saveLoginToggle.isOn,
+                () =>
+                {
+                    SceneManager.LoadSceneAsync("chat", LoadSceneMode.Additive);
+                    SceneManager.UnloadSceneAsync("auth");
+                },
+                reason =>
+                {
+                    _errorText.text = $"Authentication Failed: ${reason}";
+                    SetInteractable(true);
+                }
+            );
         }
 #endregion
     }
