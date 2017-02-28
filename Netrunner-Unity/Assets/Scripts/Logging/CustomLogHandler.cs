83e3e76aa9fa0ed49da1fe8a754df693ebe45624
Shane Lillie <ignignokterr@gmail.com>
2017-02-28 08:46:03 -0800

    Useful utils

---
+++ Netrunner-Unity/Assets/Scripts/Logging/CustomLogHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+using UnityEngine;
+
+namespace EnergonSoftware.Netrunner.Logging
+{
+    public sealed class CustomLogHandler : ILogHandler
+    {
+// TODO: this should take the type of the logger in order to tag the log
+
+        public void LogFormat(LogType logType, UnityEngine.Object context, string format, params object[] args)
+        {
+            StringBuilder newLogFormat = new StringBuilder();
+
+            newLogFormat.Append("[");
+            if(AsyncTools.IsMainThread()) {
+                string contextName = (SynchronizationContext.Current as UnitySynchronizationContext)?.Name ?? "-";
+                newLogFormat.Append($"{contextName}:{Time.frameCount}");
+            } else {
+                newLogFormat.Append($"{Thread.CurrentThread.ManagedThreadId}");
+            }
+            newLogFormat.Append("]");
+
+            newLogFormat.Append(": ");
+            newLogFormat.Append(format);
+
+            Debug.logger.logHandler.LogFormat(logType, context, newLogFormat.ToString(), args);
+        }
+
+        public void LogException(Exception exception, UnityEngine.Object context)
+        {
+            Debug.logger.LogException(exception, context);
+        }
+    }
+}
---
+++ Netrunner-Unity/Assets/Scripts/Logging/LoggerExtensions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace EnergonSoftware.Netrunner.Logging
+{
+    public static class LoggerExtensions
+    {
+        public static void LogWarning(this Logger logger, string message)
+        {
+            logger.Log(LogType.Warning, message);
+        }
+
+        public static void LogError(this Logger logger, string message)
+        {
+            logger.Log(LogType.Error, message);
+        }
+    }
+}
