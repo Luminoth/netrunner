dfe9192a30061ca3675e03c39619c386dbd0200e
Shane Lillie <ignignokterr@gmail.com>
2017-02-28 12:07:41 -0800

    Util classes

---
+++ Netrunner-Unity/Assets/Scripts/Core/Util/PlayerPrefsExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace EnergonSoftware.Netrunner.Core.Util
+{
+    public static class PlayerPrefsExtensions
+    {
+        public static void SetBool(string key, bool value)
+        {
+            PlayerPrefs.SetInt(key, value ? 1 : 0);
+        }
+
+        public static bool GetBool(string key)
+        {
+            return 0 != PlayerPrefs.GetInt(key);
+        }
+
+        public static bool GetBool(string key, bool defaultValue)
+        {
+            return 0 != PlayerPrefs.GetInt(key, defaultValue ? 1 : 0);
+        }
+    }
+}
