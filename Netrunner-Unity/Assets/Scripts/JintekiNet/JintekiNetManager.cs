787bdee31e3cfbd4c13280b6060e8971b0f2b354
Shane Lillie <ignignokterr@gmail.com>
2017-04-19 11:07:22 -0700

    Cleanup

--- Netrunner-Unity/Assets/Scripts/JintekiNet/JintekiNetManager.cs
+++ Netrunner-Unity/Assets/Scripts/JintekiNet/JintekiNetManager.cs
@@ -4,10 +4,6 @@ using EnergonSoftware.Netrunner.Core;
 
 namespace EnergonSoftware.Netrunner.JintekiNet
 {
-// jinteki uses socket.io according to the Chrome browser tools
-// the Unity Socket.IO plugin apparently requires Unity Pro :\
-// so maybe building something like that from scratch is needed
-
     public sealed class JintekiNetManager : SingletonBehavior<JintekiNetManager>
     {
         public void Authenticate(string username, string password, Action onSuccess, Action<string> onFailure)
