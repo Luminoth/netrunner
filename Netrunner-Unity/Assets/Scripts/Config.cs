df2efcb8790380c810784297bdd65b29d1641023
Shane Lillie <ignignokterr@gmail.com>
2017-04-24 21:32:16 -0700

    Removed unnecessary menu item

--- Netrunner-Unity/Assets/Scripts/Config.cs
+++ Netrunner-Unity/Assets/Scripts/Config.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 
-using EnergonSoftware.Netrunner.Core.Util;
-
 using UnityEngine;
 
 namespace EnergonSoftware.Netrunner
@@ -10,14 +8,6 @@ namespace EnergonSoftware.Netrunner
     [Serializable]
     public sealed class Config : ScriptableObject
     {
-#region UNITY_EDITOR
-        [UnityEditor.MenuItem("Assets/Create/Energon Software/Netrunner/Config")]
-        private static void Create()
-        {
-            ScriptableObjectUtil.CreateAsset<Config>();
-        }
-#endregion
-
         [SerializeField]
         private string _backendURL;
 
