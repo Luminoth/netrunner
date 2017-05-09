cdf3d628d9a51211d0004cec0ab61ddc84b44ca2
Shane Lillie <ignignokterr@gmail.com>
2017-05-08 18:51:58 -0700

    Path utils

---
+++ Netrunner-Unity/Assets/Scripts/Core/Util/PathUtil.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace EnergonSoftware.Netrunner.Core.Util
+{
+    public static class PathUtil
+    {
+        public static string Combine(string a, string b)
+        {
+            return a + "/" + b;
+        }
+
+        public static string Combine(string a, string b, string c)
+        {
+            return a + "/" + b + "/" + c;
+        }
+
+        public static string GetDataPath(string relativePath)
+        {
+            return Combine(Application.dataPath, relativePath);
+        }
+    }
+}
